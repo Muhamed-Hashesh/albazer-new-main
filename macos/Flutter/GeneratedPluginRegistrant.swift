2ddab55a8c7590960dd2ab011818892b758205c6
MohamedElsay0d <me827794@gmail.com>
2025-07-07 01:33:35 +0300

    Update project configuration files and dependencies, including the addition of new CMake files for multiple architectures (arm64-v8a, armeabi-v7a, x86, x86_64) and the removal of the settings.gradle.kts file. Update pubspec.yaml to reflect the latest version of the skeletonizer package.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,7 +17,6 @@ import geolocator_apple
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
-import sign_in_with_apple
 import sqflite
 import url_launcher_macos
 
@@ -34,7 +33,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
